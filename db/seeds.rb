7d5602860c9500cd29926e8917ea68a39c414149
Miles Smith <miles@vimae.com>
2013-08-23 14:03:38 -0700

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,8 @@ puts "Seeding database"
 puts "-------------------------------"
 
 # Create an initial Admin User
-admin_username = "errbit"
-admin_email = "errbit@#{Errbit::Config.host}"
+admin_username = "wedtm"
+admin_email = "miles@vimae.com"
 admin_pass  = 'password'
 
 puts "Creating an initial admin user:"
